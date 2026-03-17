fb4722eafaf4acf2db4e3b3cd5ebd27b73e574fe
rogersoubr <rogersoubrand1a@gmail.com>
2026-03-17 19:52:43 -0300

    :ok_hand: style: Recebendo palavra e gitignore

--- VPL02/wordle.cpp
+++ VPL02/wordle.cpp
@@ -6,12 +6,22 @@ using namespace std; //esse danado que faz nao precisar usar std::
 
 int main(){
     string linha;
+    string chave[41];
+    int escolhida;
     ifstream in("palavras.txt", fstream::in);
     if(!in.is_open()){
         return 1;
     }
-    getline(in, linha);
-    cout << linha <<endl;
+    cout << "Digite o número da palavra: ";
+    cin >> escolhida;
+    for(int i = 0; i<= escolhida; i++){
+        getline(in, chave[i]);
+    }
+
+    for(int i = 1; i<= escolhida; i++){
+        cout << chave[i] << endl;//nao usei getline aqui porque get pega
+    }
+    cout << linha << endl;
 
     in.close();
 
