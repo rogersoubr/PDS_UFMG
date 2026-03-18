3f82ff099fea1a1494cd76481f4c462e45a2727f
rogersoubr <rogersoubrand1a@gmail.com>
2026-03-17 21:55:27 -0300

    :recycle: refactor: impressão principal

--- VPL02/wordle.cpp
+++ VPL02/wordle.cpp
@@ -1,3 +1,4 @@
+//jogo wordle - Roger Souza Brandão 2025108154
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -6,24 +7,78 @@ using namespace std; //esse danado que faz nao precisar usar std::
 
 int main(){
     string linha;
-    string chave[41];
+    string chave[41];//agora tu podes criar array de string pae
+    string jogada;
+    string resp; //para ir incrementando com a resposta do jogo espaco(MAIUSCULO)
+    int tentativa=0;
     int escolhida;
-    ifstream in("palavras.txt", fstream::in);
-    if(!in.is_open()){
+    ifstream arqv("palavras.txt", fstream::in);
+    if(!arqv.is_open()){
         return 1;
     }
-    cout << "Digite o número da palavra: ";
+    cout << "Digite o numero da palavra [1,40]: ";
     cin >> escolhida;
     for(int i = 0; i<= escolhida; i++){
-        getline(in, chave[i]);
+        getline(arqv, chave[i]);
     }
 
-    for(int i = 1; i<= escolhida; i++){
-        cout << chave[i] << endl;//nao usei getline aqui porque get pega
-    }
-    cout << linha << endl;
+    while(tentativa <= 5){
+        cout << "Chute uma palavra maiuscula: ";
+        cin >> jogada;
 
-    in.close();
+        if(jogada != chave[escolhida]){
+            for(int i = 0; i <= 5; i++){
+                
+                for(int j = 0; j <= 5; j++){
+                    //if para cout nas letra na mesma posição
+                    if(jogada[i] == chave[escolhida][j] && j == i){//mesmo sendo string consigo pegar o lugar da letra
+                        cout << jogada[j];
+                        break;
+                    }else if(jogada[i] == chave[escolhida][j]){
+                        cout << (char)((int)jogada[i] + 32);
+                        break;
+                    }
+                    else{
+                        //aqui coloco o *, mais tranquilo
+                        cout << "*";
+                        break;
+                    }
+                }
+            }
+            /*
+            
+            //faço um outro if para verifiar se a letra EM VISAO do i existe e imprimir entre espaco(MAIUSCULO)
+            for(int k = 0; k <= 5; k++){
+                for(int l = 0; l <= 5; l++){
+                    if(jogada[k] != chave[escolhida][l]){
+                                    
+                    }
+                }
+            }
+            */
+
+        tentativa++;        
+        }
+        
+        else{
+            cout << "GANHOU!";
+        }
+        
+    }
+    cout << "PERDEU! " << chave[escolhida];
+
+    /*
+    cout << (int)jogada[0] << endl;
+    cout << ((int)jogada[0] + 32) << endl;
+    cout << (char)((int)jogada[0] + 32) << endl;
+    cout << chave[escolhida][0] << endl;
+    */
+
+
+
+
+
+    arqv.close();
 
     return 0;
 }
