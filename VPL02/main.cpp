8297343a99bc5c03762c5af59286168bfca54eab
rogersoubr <rogersoubr@gmail.com>
2026-08-12 10:26:24 -0300

    separando string com getline

--- VPL02/main.cpp
+++ VPL02/main.cpp
@@ -7,31 +7,57 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <cstdlib>
 #include <cstring>
 using namespace std;
 
 
 int main(){
-    int pp_num;
-    string pp_full;
-    string pp;
-    string coment;
+    int pp_num;//numero de palavras proibidas
+    string pp_full;//palavras proibidas totais
+    string pp[100];//palavras proibidas
+    string cmt_full;//comentario completos
+    string cmt[100];//comentarios
 
     cout<<"digite as palavra proibida (quantidade palavra1 palavra2...): ";
-    cin >> pp_full;
-
-
-    for(int i=0; pp_full[i] != '\0'; i++){
-        cout<<"DEPURACAO"<<endl;
-        if(isspace(pp_full[i])){
-            cout<<"achei um espaco";
-        };
+    cin >> pp_num;
+    getline(cin, pp_full);
+    stringstream palavras_proibidas(pp_full);
+    string auxpp;//nao posso colocar uma matriz de char no getline ):
+        int i =0;
+        while(getline(palavras_proibidas, auxpp, ' ')) {
+            if(auxpp != ""){
+                pp[i] = auxpp;
+                i++;
+            }
+        }
+    if (pp_num != i){
+        cout<<"\nerro, quantidade de palavras proibidas incoerentes"<<endl;
+        return 1;
     }
     
-
     cout<<"digite o comentario: ";
-    cin >> coment;
+    getline(cin, cmt_full);
+    stringstream comentarios_completos(cmt_full);
+    string auxcmt;
+    int cmt_num =0;
+    while(getline(comentarios_completos, auxcmt, ' ')) {
+        if(auxcmt != ""){
+            cmt[cmt_num] = auxcmt;
+            cmt_num++;
+        }
+    }
+    
+    // for (int i = 0; i < count; i++){
+    //     /* code */
+    //     for (int k = 0; k <= j; k++){
+    //         if(cmt[k])
+    //     }
+    // }
+    
+
+    
 
     return 0;
 }
---
+++ testes_aulas/string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main(){
+
+    string line;
+    cin >> line;
+    cout << line[0] <<endl;
+
+    return 0;
+}
